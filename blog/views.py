6d84e822dec3a34250a8ce4ab009e99929e35b2e
Istvan28 <160413679+Istvan28@users.noreply.github.com>
2024-08-28 20:00:07 +0200

    step2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Szia")
+
+def myfeed(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("My_FEED")
+
+def article_id(request: HttpRequest) -> HttpResponse:
+    return ("Article_ID")
+
